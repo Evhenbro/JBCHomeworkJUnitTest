4bc2fe76cdc9ac6f93578671336be6a722112e7f
Yevhen Yarema <evhenbro@gmail.com>
2019-02-18 21:36:57 +0200

    add FizzBuzz class

---
+++ src/main/java/com/yarema/FizzBuzz.java
@@ -0,0 +1,4 @@
+package com.yarema;
+
+public class FizzBuzz {
+}
--- src/test/java/com/yarema/FizzBuzzTest.java
+++ src/test/java/com/yarema/FizzBuzzTest.java
@@ -1,6 +1,9 @@
 package com.yarema;
 
+import org.junit.Test;
+
 public class FizzBuzzTest {
 
+    private static FizzBuzz unit;
 
 }
