8f10923aee78ca0ef4818b471edc30143f85d651
Yevhen Yarema <evhenbro@gmail.com>
2019-02-18 23:07:21 +0200

    write a little bit of body to method printList for Fizz and Buzz

--- src/main/java/com/yarema/FizzBuzz.java
+++ src/main/java/com/yarema/FizzBuzz.java
@@ -7,7 +7,9 @@ public class FizzBuzz {
     public ArrayList<String> printList(ArrayList<Integer> lists) {
         ArrayList<String> arrayList = new ArrayList<String>();
         for (Integer list: lists) {
-            if ((list % 3) == 0.0) {
+            if ((list % 5) == 0.0) {
+                arrayList.add("Buzz");
+            } else if ((list % 3) == 0.0) {
                 arrayList.add("Fizz");
             } else
                 arrayList.add("" + list);
