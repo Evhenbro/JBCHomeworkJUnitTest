53c257a5a1f84fafafae9e686276ffbf8396cd90
Yevhen Yarema <evhenbro@gmail.com>
2019-02-18 22:05:56 +0200

    add test methodPrintListIsCorrectListReturn

--- src/main/java/com/yarema/FizzBuzz.java
+++ src/main/java/com/yarema/FizzBuzz.java
@@ -1,4 +1,14 @@
 package com.yarema;
 
+import java.util.ArrayList;
+
 public class FizzBuzz {
+
+    public ArrayList<String> printList(ArrayList<Integer> lists) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+        for (Integer list: lists) {
+
+        }
+        return arrayList;
+    }
 }
--- src/test/java/com/yarema/FizzBuzzTest.java
+++ src/test/java/com/yarema/FizzBuzzTest.java
@@ -1,9 +1,18 @@
 package com.yarema;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FizzBuzzTest {
 
     private static FizzBuzz unit;
+
+    @Test
+    public void methodPrintListIsCorrectListReturn() {
+        Assert.assertEquals(new ArrayList<String>(Arrays.asList("1", "2")), unit.printList(new ArrayList<Integer>(Arrays.asList(1, 2))));
+    }
 
 }
