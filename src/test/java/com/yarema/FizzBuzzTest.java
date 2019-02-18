823cf44ec7dea9e519ef5ed1c28bebfa8cabcc4a
Yevhen Yarema <evhenbro@gmail.com>
2019-02-18 23:10:39 +0200

    write test for substituting multiples of 15 to "FizzBuzz"

--- src/test/java/com/yarema/FizzBuzzTest.java
+++ src/test/java/com/yarema/FizzBuzzTest.java
@@ -30,4 +30,9 @@ public class FizzBuzzTest {
     public void methodIsCorrectNumberSubstituteToFizzAndBuzz() {
         Assert.assertEquals(new ArrayList<String>(Arrays.asList("1", "2", "4", "Buzz")), unit.printList(new ArrayList<Integer>(Arrays.asList(1, 2, 4, 5))));
     }
+
+    @Test
+    public void methodIsCorrectNumberSubstituteToFizzAndBuzzAndFizzBuzz() {
+        Assert.assertEquals(new ArrayList<String>(Arrays.asList("1", "Fizz", "4", "Buzz", "FizzBuzz")), unit.printList(new ArrayList<Integer>(Arrays.asList(1, 3, 4, 5, 15))));
+    }
 }
